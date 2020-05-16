1e12b158e1c793a863133d60f7c2c4674e708df5
rollrat <rollrat.cse@gmail.com>
2020-05-16 12:34:04 +0900

    Archive 프로젝트 추가

---
+++ GalleryArchive/Program.cs
@@ -0,0 +1,15 @@
+﻿// This source code is a part of Gallery Explorer Project.
+// Copyright (C) 2020. rollrat. Licensed under the MIT Licence.
+
+using System;
+
+namespace GalleryArchive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
